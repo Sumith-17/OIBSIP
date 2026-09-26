f526ba6854442fa0b8605b85f7f6e5e54fd92866
Sumith_Kumar <sumithkumar1700@gmail.com>
2026-09-26 11:33:01 +0530

    changes in js for complete working logic

--- WebDev-L2-Calculator/calci.js
+++ WebDev-L2-Calculator/calci.js
@@ -1,5 +1,9 @@
 let buttonEl=document.querySelectorAll(".keys button")
 const displayEl=document.getElementById("result")
+
+let displayExpression=""
+let expression=""
+
 buttonEl.forEach((button) => {
     button.addEventListener("click", () => {
         console.log(button.textContent);
@@ -15,11 +19,23 @@ buttonEl.forEach((button) => {
         else if(value==="=")
         {
             calculate()
+            
         }
         else
         {
-            
-            displayEl.textContent+=button.textContent
+            if(value==="x")
+            {
+                expression+="*"
+            }
+            else if(value==="÷")
+            {
+                expression+="/"
+            }
+            else
+                expression+=value
+            displayExpression+=value
+            console.log(expression)
+            displayEl.textContent=displayExpression
         }
         
     });
@@ -27,11 +43,86 @@ buttonEl.forEach((button) => {
 
 function clearAll()
 {
-    displayEl.textContent=""
+    expression = "";
+    displayExpression=""
+    displayEl.textContent = "";
+}
+
+
+function calculate()
+{
+    let tokens=[]
+    let number=""
+
+    for(let i=0;i<expression.length;i++)
+    {
+        let char=expression[i]
+        if(char==="+" || char==="-" || char==="*" || char==="/")
+        {
+            tokens.push(number)
+            number=""
+            tokens.push(char)
+        }
+        else
+        {
+            number+=char;
+        }
+    }
+    if(number!=="")
+    {
+        tokens.push(number);
+    }
+    console.log(tokens)
+
+    
+
+    let newTokens=[]
+    for(let i=0;i<tokens.length;i++)
+    {
+
+        let currNum=tokens[i]
+        
+        if(currNum==="*" || currNum==="/")
+        {
+            let prevNum=Number(newTokens[newTokens.length-1])
+            let nextNum=Number(tokens[i+1])
+            //how to place the result in right position 
+            let result;
+            if(currNum==="*")
+                result=prevNum * nextNum
+            else 
+                result=prevNum / nextNum
+
+            newTokens[newTokens.length-1] = result;
+
+            i++;
+        }
+        else{
+            newTokens.push(currNum)
+        }
+    }
+    console.log("new Tokens after * /:",newTokens)
+    let result=Number(newTokens[0])
+    for(let i=1;i<newTokens.length;i+=2)
+    {
+        let operator=newTokens[i]
+        let nextNum=Number(newTokens[i+1])
+            if(operator==="+")
+                result+=nextNum
+            else if(operator==='-')
+                result -=nextNum
+
+    }
+    console.log("final result:",result)
+    expression=String(result);
+    displayExpression=String(result);
+
+    displayEl.textContent=displayExpression
+
 }
 function back()
 {
-    let text=displayEl.textContent
-    displayEl.textContent=text.slice(0,-1)
+    expression=expression.slice(0,-1)
+    displayExpression = displayExpression.slice(0,-1)
+    displayEl.textContent=displayExpression
 }
- 
